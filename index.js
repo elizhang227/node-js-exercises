9400975f6beaf2bdc038226b7f7f1dcd17c13b45
elizhang227 <zhang.eli@hotmail.com>
2019-05-24 09:37:42 -0400

    uploaded practice node js files

---
+++ index.js
@@ -0,0 +1,13 @@
+const messages = require('./messages');
+const log = require('./logger');
+
+console.log(messages);
+
+log.info('Node is running');
+log.info('Is nodemon working?');
+// example of error case
+// log.error('Everything is on fire');
+
+app.get('/login', () => {
+    
+})
